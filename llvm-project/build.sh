f918592b70f25dcf88a5fd72e073136ae62d218f
Nick Desaulniers <ndesaulniers@google.com>
2022-05-16 15:01:04 -0700

    change llvm-project tag

--- llvm-project/build.sh
+++ llvm-project/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 wget --no-clobber https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.1/llvm-project-14.0.1.src.tar.xz
-docker build --tag clangbuiltlinux/containers:llvm-project . -f Dockerfile.bootstrap
-#docker create --name llvm-project clangbuiltlinux/containers:llvm-project
+docker build --tag clangbuiltlinux/llvm-project:latest . -f Dockerfile.bootstrap
 docker cp llvm-project:/usr/local/bin/clang-14 clang
-
