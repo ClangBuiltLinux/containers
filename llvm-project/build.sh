92ac35d940d1e627b0db0b1f8995e7499e7a7cfb
Nick Desaulniers <ndesaulniers@google.com>
2022-05-17 10:22:50 -0700

    Revert "rename to Containerfile; then we don't need -f and CI can build"
    This reverts commit fc4ee684d5996fc8547518b40fc9ca085740262f.
    
    Unnecessary after
    https://github.com/ClangBuiltLinux/containers/pull/6

--- llvm-project/build.sh
+++ llvm-project/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 wget --no-clobber https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.1/llvm-project-14.0.1.src.tar.xz
-docker build --tag clangbuiltlinux/llvm-project:latest .
+docker build --tag clangbuiltlinux/llvm-project:latest . -f Dockerfile.bootstrap
 docker cp llvm-project:/usr/local/bin/clang-14 clang
