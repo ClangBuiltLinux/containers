3a5806e47c0beae0bbf71f6cd8fbb0749cc4778c
Nick Desaulniers <ndesaulniers@google.com>
2022-05-20 09:59:02 -0700

    add comments to cmake vars

--- llvm-project/stage1.cmake
+++ llvm-project/stage1.cmake
@@ -1,45 +1,134 @@
-# TODO: comment every one of these.
+# Please try to keep these cmake variables alphabetically sorted.
+
+# Enable optimizations, as opposed to a debug build.
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+# Use Alpine's clang and clang++ from their clang package as the stage0
+# compilers.
 set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE FILEPATH "")
 set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE FILEPATH "")
+
+# Alpine's clang does not default to LLD; it defaults to BFD. Use LLD when
+# building executables and shared libraries.
 set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "")
 set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld" CACHE STRING "")
+
+# Set the default target triple to match the host.
+# TODO: passing in the value of $(clang -print-multiarch) causes failures.
+# It seems that alpine clang's default target triple is x86_64-linux-gnu.
+# Perhaps missing alpine in the triple causes some incompatibility?
 set(LLVM_DEFAULT_TARGET_TRIPLE "x86_64-alpine-linux-musl" CACHE STRING "")
+
+# Use Alpine's lld as the stage0 linker to link everything.
 set(LLVM_ENABLE_LLD ON CACHE BOOL "")
+
+# For stage 1, just build clang and LLD, which are used by the "runtimes build"
+# to build the runtime libraries of LLVM.
 set(LLVM_ENABLE_PROJECTS "clang;lld;" CACHE STRING "")
+
+# LLVM "runtimes" build.
 set(LLVM_ENABLE_RUNTIMES "compiler-rt;libcxx;libcxxabi;libunwind;" CACHE STRING "")
+
+# FORCE_ON causes the build to fail if zlib is not found in the environment
+# during configuration, rather than much later during link.
 set(LLVM_ENABLE_ZLIB "FORCE_ON" CACHE STRING "")
+
+# Just build stage1 to target the host. It's not the end product, so it won't
+# be able to target all of the kernel targets we can build.
 set(LLVM_TARGETS_TO_BUILD "host;" CACHE STRING "")
+
+# Stage1 clang should default to lld rather than BFD.
+# TODO: it seems this was ignored by the runtimes build...
+# otherwise why would CMAKE_EXE_LINKER_FLAGS be necessary?
 set(CLANG_DEFAULT_LINKER "lld" CACHE STRING "")
+# Stage1 clang should default to compiler-rt rather than libgcc.
 # TODO: it seems this was ignored by the runtimes build...
 set(CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
+
+# Disable arc migrate. We don't use that, ever.
 set(CLANG_ENABLE_ARCMT OFF CACHE BOOL "")
+
+# Disable static analyzer. Don't need it for stage1.
 set(CLANG_ENABLE_STATIC_ANALYZER OFF CACHE BOOL "")
+
+# Disable plugin support. Don't need it, ever.
 set(CLANG_PLUGIN_SUPPORT OFF CACHE BOOL "")
+
+# The compiler builtins are necessary.
 set(COMPILER_RT_BUILD_BUILTINS ON CACHE BOOL "")
+
+# GWP ASAN fails to build without libexecinfo-dev. Don't need it for stage1.
 set(COMPILER_RT_BUILD_GWP_ASAN OFF CACHE BOOL "")
+
+# Don't need libfuzzer, ever.
 set(COMPILER_RT_BUILD_LIBFUZZER OFF CACHE BOOL "")
+
+# Don't need memprof, ever.
 set(COMPILER_RT_BUILD_MEMPROF OFF CACHE BOOL "")
+
+# Don't need ORC, ever.
 set(COMPILER_RT_BUILD_ORC OFF CACHE BOOL "")
+
+# Disable profiling support. Not necessary for stage1.
 set(COMPILER_RT_BUILD_PROFILE OFF CACHE BOOL "")
+
+# Disable sanitizer support. Not necessary for stage1.
 set(COMPILER_RT_BUILD_SANITIZERS OFF CACHE BOOL "")
+
+# Don't need xray.
 set(COMPILER_RT_BUILD_XRAY OFF CACHE BOOL "")
+
+# Compiler-rt is meant as a replacement to libgcc.
 set(COMPILER_RT_HAS_GCC_S_LIB OFF CACHE BOOL "")
+
+# Who needs docs anyways? Can't RTFM if there is no FM.
 set(LIBUNWIND_INCLUDE_DOCS OFF CACHE BOOL "")
+
+# We don't plan to run the tests, disable them.
 set(LIBUNWIND_INCLUDE_TESTS OFF CACHE BOOL "")
+
+# We need the headers available when we install-libunwind.
 set(LIBUNWIND_INSTALL_HEADERS ON CACHE BOOL "")
+
+# Libunwind should use compiler-rt rather than libgcc.
 set(LIBUNWIND_USE_COMPILER_RT ON CACHE BOOL "")
+
+# TODO: I forget why I enabled this.
 set(LIBCXXABI_ENABLE_STATIC_UNWINDER ON CACHE BOOL "")
+
+# We don't plan to run the tests; don't build them.
 set(LIBCXXABI_INCLUDE_TESTS OFF CACHE BOOL "")
+
+# We want libc++abi to use compiler-rt.
 set(LIBCXXABI_USE_COMPILER_RT ON CACHE BOOL "")
+
+# We want libc++abi to use LLVM's libunwind.
 set(LIBCXXABI_USE_LLVM_UNWINDER ON CACHE BOOL "")
+
+# We want libc++ to use libc++abi.
 set(LIBCXX_CXX_ABI libcxxabi CACHE BOOL "")
+
+# We don't need C++ experimental APIs.
 set(LIBCXX_ENABLE_EXPERIMENTAL_LIBRARY OFF CACHE BOOL "")
+
+# We want libc++ to use compiler-rt. If we don't disable libatomic explicitly,
+# libc++ will try to link against it.
 set(LIBCXX_HAS_ATOMIC_LIB OFF CACHE BOOL "")
 set(LIBCXX_HAS_GCC_LIB OFF CACHE BOOL "")
 set(LIBCXX_HAS_GCC_S_LIB OFF CACHE BOOL "")
+
+# This is critical; libc++ depends on a libc. This MUST be set when building
+# against musl otherwise expect libc++ build failures related to locales.
 set(LIBCXX_HAS_MUSL_LIBC ON CACHE BOOL "")
+
+# We don't plan to run the benchmarks, so don't build them.
 set(LIBCXX_INCLUDE_BENCHMARKS OFF CACHE BOOL "")
+
+# Don't build docs.
 set(LIBCXX_INCLUDE_DOCS OFF CACHE BOOL "")
+
+# Don't build tests.
 set(LIBCXX_INCLUDE_TESTS OFF CACHE BOOL "")
+
+# We wan't libc++ to use compiler-rt.
 set(LIBCXX_USE_COMPILER_RT ON CACHE BOOL "")
--- llvm-project/stage2.cmake
+++ llvm-project/stage2.cmake
@@ -1,22 +1,74 @@
-# TODO: comment every one of these.
+# Please try to keep these cmake variables alphabetically sorted.
+
+# Enable optimizations, as opposed to a debug build.
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+# Use stage1's clang to bootstrap stage2, as opposed to Alpine's clang (which
+# shouldn't be installed on the system at this point, but would be installed to
+# /usr/bin/clang).
 set(CMAKE_CXX_COMPILER "/usr/local/bin/clang++" CACHE FILEPATH "")
+
+# Use libc++ from stage1.
+# We could not do this in stage1 because Alpine doesn't distribue libc++ ATM.
+# TODO: does clang cmake not have a CLANG_DEFAULT_* flag for setting this?
 set(CMAKE_CXX_FLAGS "--stdlib=libc++" CACHE STRING "")
+
+# See above comment for CMAKE_CXX_COMPILER.
 set(CMAKE_C_COMPILER "/usr/local/bin/clang" CACHE FILEPATH "")
+
+# Use libunwind from stage1.
+# Statically link resulting executable.
+# TODO: why is -lc++abi necessary for static link?
 set(CMAKE_EXE_LINKER_FLAGS "--unwindlib=libunwind -static -lc++abi" CACHE STRING "")
 set(CMAKE_SHARED_LINKER_FLAGS "--unwindlib=libunwind" CACHE STRING "")
+
+# Set the default target triple to match the host.
+# TODO: passing in the value of $(clang -print-multiarch) causes failures.
+# It seems that alpine clang's default target triple is x86_64-linux-gnu.
+# Perhaps missing alpine in the triple causes some incompatibility?
 set(LLVM_DEFAULT_TARGET_TRIPLE x86_64-alpine-linux-musl CACHE STRING "")
+
+# Use libc++ from stage1.
+# TODO: is CMAKE_CXX_FLAGS still necessary if this is set?
 set(LLVM_ENABLE_LIBCXX ON CACHE BOOL "")
+
+# Use lld from stage1.
 set(LLVM_ENABLE_LLD ON CACHE BOOL "")
+
+# Just build clang and lld for now.
 set(LLVM_ENABLE_PROJECTS "clang;lld" CACHE STRING "")
+
+# FORCE_ON causes the build to fail if zlib is not found in the environment
+# during configuration, rather than much later during link.
 set(LLVM_ENABLE_ZLIB "FORCE_ON" CACHE STRING "")
+
+# This is necessary to statically link libc++ into clang.
 set(LLVM_STATIC_LINK_CXX_STDLIB "1" CACHE STRING "")
+
+# Just build support for x86 for now.
+# TODO: change this to host when adding more stages.
 set(LLVM_TARGETS_TO_BUILD "X86;" CACHE STRING "")
+
+# Set clang's default --stdlib= to libc++.
 set(CLANG_DEFAULT_CXX_STDLIB "libc++" CACHE STRING "")
+
+# Set clang's default -fuse-ld= to lld.
 set(CLANG_DEFAULT_LINKER "lld" CACHE STRING "")
+
+# Have clang default to llvm-objcopy.
 set(CLANG_DEFAULT_OBJCOPY "llvm-objcopy" CACHE STRING "")
+
+# Set clang's default --rtlib= to compiler-rt.
 set(CLANG_DEFAULT_RTLIB "compiler-rt" CACHE STRING "")
+
+# Set clang's default --unwindlib= to libunwind.
 set(CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
+
+# Disable arc migrate. We don't use that, ever.
 set(CLANG_ENABLE_ARCMT OFF CACHE BOOL "")
+
+# Disable static analyzer. Don't need it for stage2.
 set(CLANG_ENABLE_STATIC_ANALYZER OFF CACHE BOOL "")
+
+# Disable plugin support. Don't need it, ever.
 set(CLANG_PLUGIN_SUPPORT OFF CACHE BOOL "")
