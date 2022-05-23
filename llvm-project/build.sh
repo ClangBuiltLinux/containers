9ee3337c4a6b156dccad0492ec85f072ab705517
Nick Desaulniers <ndesaulniers@google.com>
2022-05-23 14:12:58 -0700

    replace build.sh with Makefile

--- llvm-project/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-docker build --tag clangbuiltlinux/llvm-project:latest .
-#docker create --name llvm-project clangbuiltlinux/llvm-project:latest
-#docker cp llvm-project:/usr/local/bin/clang-14 clang
-dot -Tsvg stage1.dot > stage1.svg
-dot -Tsvg stage2.dot > stage2.svg
