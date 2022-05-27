efa95d72aaeb3f6ec1f182136d1427d9b42b0aa1
Nick Desaulniers <ndesaulniers@google.com>
2022-05-27 15:46:41 -0700

    update stage3 cmake comments

--- llvm-project/stage3.cmake
+++ llvm-project/stage3.cmake
@@ -3,30 +3,12 @@
 # Enable optimizations, as opposed to a debug build.
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
 
-# Use stage1's clang to bootstrap stage2, as opposed to Alpine's clang (which
-# shouldn't be installed on the system at this point, but would be installed to
-# /usr/bin/clang).
-#set(CMAKE_CXX_COMPILER "/usr/local/bin/clang++" CACHE FILEPATH "")
-
-# Use libc++ from stage1.
-# We could not set CLANG_DEFAULT_CXX_STDLIB in stage1 because Alpine doesn't
-# distribute libc++ ATM.
-# Even though we later will set LLVM_ENABLE_LIBCXX, cmake will fail thinking
-# that "The C++ compiler is not able to compile a simple test program." As with
-# stage1, this is an unfortunate effect of Alpine packaging the stdlibc++
-# headers in the g++ package, rather than the libstdc++ package. In stage1, we
-# used those headers; in stage2 we do not.
-#set(CMAKE_CXX_FLAGS "--stdlib=libc++" CACHE STRING "")
-
-# See above comment for CMAKE_CXX_COMPILER.
-#set(CMAKE_C_COMPILER "/usr/local/bin/clang" CACHE FILEPATH "")
-set(CMAKE_C_FLAGS "--sysroot=/sysroot" CACHE STRING "")
+# Use the sysroot we've been building up.
 set(CMAKE_CXX_FLAGS "--sysroot=/sysroot" CACHE STRING "")
+set(CMAKE_C_FLAGS "--sysroot=/sysroot" CACHE STRING "")
 
-# Use libunwind from stage1.
 # Statically link resulting executable.
 set(CMAKE_EXE_LINKER_FLAGS "-static -lc++abi" CACHE STRING "")
-#set(CMAKE_SHARED_LINKER_FLAGS "--unwindlib=libunwind" CACHE STRING "")
 
 # Set the default target triple to match the host.
 # TODO: passing in the value of $(clang -print-multiarch) causes failures.
@@ -34,11 +16,11 @@ set(CMAKE_EXE_LINKER_FLAGS "-static -lc++abi" CACHE STRING "")
 # Perhaps missing alpine in the triple causes some incompatibility?
 set(LLVM_DEFAULT_TARGET_TRIPLE x86_64-alpine-linux-musl CACHE STRING "")
 
-# Use libc++ from stage1.
+# Use libc++ from stage2.
 # TODO: is CMAKE_CXX_FLAGS still necessary if this is set?
 set(LLVM_ENABLE_LIBCXX ON CACHE BOOL "")
 
-# Use lld from stage1.
+# Use lld from stage2.
 set(LLVM_ENABLE_LLD ON CACHE BOOL "")
 
 # Just build clang and lld for now.
@@ -53,7 +35,7 @@ set(LLVM_STATIC_LINK_CXX_STDLIB "1" CACHE STRING "")
 
 # Just build support for x86 for now.
 # TODO: change this to host when adding more stages.
-set(LLVM_TARGETS_TO_BUILD "X86;" CACHE STRING "")
+set(LLVM_TARGETS_TO_BUILD "host;" CACHE STRING "")
 
 # Set clang's default --stdlib= to libc++.
 set(CLANG_DEFAULT_CXX_STDLIB "libc++" CACHE STRING "")
@@ -73,7 +55,7 @@ set(CLANG_DEFAULT_UNWINDLIB "libunwind" CACHE STRING "")
 # Disable arc migrate. We don't use that, ever.
 set(CLANG_ENABLE_ARCMT OFF CACHE BOOL "")
 
-# Disable static analyzer. Don't need it for stage2.
+# Disable static analyzer. Don't need it for stage3.
 set(CLANG_ENABLE_STATIC_ANALYZER OFF CACHE BOOL "")
 
 # Disable plugin support. Don't need it, ever.
