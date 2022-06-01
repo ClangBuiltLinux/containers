989e2becf62da58ad0d287f4957c04e5c51f7573
Nathan Chancellor <nathan@kernel.org>
2022-06-01 15:42:56 -0700

    ci: Add script to configure virtual machine
    To prepare for self-hosted GitHub Actions, add a script to quickly
    configure a bare Fedora virtual machine after installing and configuring
    the self-hosted runner application (entire "Download" section and first
    step under "Configure" section in "Create self-hosted runner" page in
    GitHub repository settings). This will allow people other than myself to
    set up a virtual machine on their own host if I am unavailable to
    administer the existing virtual machines.
    
    To run the script in the VM (assuming a root password as opposed to
    adding the runner account to the wheel group for sudo permissions):
    
    $ su -c "./configure-vm.sh $USER"
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ ci/configure-vm.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function checks() {
+    if [[ $(id -u) -ne 0 ]]; then
+        echo "[-] This script needs to be run as root!"
+        exit 1
+    fi
+
+    if [[ $# -eq 0 ]]; then
+        echo "[-] User name of the runner user account required as first argument!"
+        exit 1
+    else
+        username=$1
+    fi
+    workspace=/home/$username/actions-runner
+
+    # shellcheck disable=SC1091
+    source /usr/lib/os-release
+    case "$ID" in
+        fedora) ;;
+        *)
+            echo "[-] $NAME has not been tested with this script!"
+            exit 1
+            ;;
+    esac
+
+    if [[ ! -f $workspace/svc.sh ]]; then
+        echo "[-] Runner must be configured already!"
+        echo '[-] Follow all "Download" steps and the first "Configure" step on the "Create self-hosted runner" page.'
+        exit 1
+    fi
+}
+
+function resize_rootfs() {
+    echo "[+] Resizing root filesystem if necessary"
+    dev_mapper=$(df -H | grep /dev/mapper/ | cut -d ' ' -f 1)
+    if [[ -n ${dev_mapper:-} ]]; then
+        # This might fail if the volume has already been resized, don't make it
+        # fatal to the script
+        lvextend -l +100%FREE "$dev_mapper" || true
+        xfs_growfs "$dev_mapper"
+    fi
+}
+
+function update_install_packages() {
+    echo "[+] Updating operating system"
+    dnf update -qy
+
+    echo "[+] Installing necessary packages"
+    dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf install -qy \
+        containerd.io \
+        docker-ce \
+        docker-ce-cli \
+        docker-compose-plugin \
+        git \
+        zstd
+}
+
+function configure_docker() {
+    echo "[+] Configuring Docker"
+    usermod -aG docker "$username"
+    systemctl enable --now docker
+}
+
+function check_docker() {
+    echo "[+] Verifying Docker works"
+    runuser -u "$username" -- docker run --rm hello-world
+}
+
+function install_runner_service() {
+    echo "[+] Installing runner systemd service"
+    cd "$workspace"
+    ./svc.sh install "$username"
+}
+
+function install_cleanup_sh() {
+    # https://docs.github.com/en/actions/hosting-your-own-runners/running-scripts-before-or-after-a-job
+    echo "[+] Setting up clean up script"
+
+    cleanup_sh=/home/$username/cleanup.sh
+    cat << 'EOF' >"$cleanup_sh"
+#!/usr/bin/env bash
+
+rm -frv "${GITHUB_WORKSPACE%/*}"
+EOF
+    chmod a+x "$cleanup_sh"
+    chown "$username:$username" "$cleanup_sh"
+
+    # Needed because the cleanup script runs in the context of the service
+    semanage fcontext --add --type initrc_exec_t "$cleanup_sh"
+    runuser -u "$username" -- restorecon -v "$cleanup_sh"
+
+    echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=$cleanup_sh" >>"$workspace"/.env
+}
+
+function runsvc_sh_selinux() {
+    # https://github.com/actions/runner/issues/1606
+    echo "[+] Adjusting SELinux context for runsvc.sh"
+    runsvc_sh=$workspace/runsvc.sh
+    semanage fcontext --add --type initrc_exec_t "$runsvc_sh"
+    runuser -u "$username" -- restorecon -v "$runsvc_sh"
+}
+
+function start_service() {
+    echo "[+] Starting GitHub Actions service"
+    ./svc.sh start
+}
+
+checks "$@"
+resize_rootfs
+update_install_packages
+configure_docker
+check_docker
+install_runner_service
+install_cleanup_sh
+runsvc_sh_selinux
+start_service
